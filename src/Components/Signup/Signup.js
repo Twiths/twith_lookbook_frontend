84c7a9a4a5203f72406cee8e692a097e43fb2bbb
Kally <kallyasmedia@gmail.com>
2021-04-03 15:17:36 +0000

    Update login structure

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import '../Login.css'
+import React, { Component } from "react";
+import "../Login.css";
 // import Header from '../Header/Header';
-import AuthService from '../../Services/AuthService'
-import Form from './Form';
-
+import AuthService from "../../Services/AuthService";
+import Form from "./Form";
+import Navigation from "../Navigation/Navigation";
 
 class Signup extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        username: '',
-        firstName: '',
-        otherName: '',
-        password: '',
-      error: ""
+      username: "",
+      firstName: "",
+      otherName: "",
+      password: "",
+      error: "",
     };
   }
 
@@ -22,42 +22,39 @@ class Signup extends Component {
     let nam = event.target.name;
     let val = event.target.value;
     this.setState({ [nam]: val });
-  }
+  };
 
   doLogin = async (event) => {
     event.preventDefault();
 
-    AuthService
-      .register(
-          this.state.username,
-          this.state.firstName,
-          this.state.otherName,
-          this.state.password
-          )
-      .then(
-        () => {
-          // this.props.hasHistory.push('/home');
-          window.location.href = "/"
-        },
-        error => {
-          console.log("signup fail: error = { " + error.toString() + " }");
-          this.setState({ error: "Can not signup successfully ! Please check username/password again" });
-        }
-      );
-  }
-
-   
-
-
+    AuthService.register(
+      this.state.username,
+      this.state.firstName,
+      this.state.otherName,
+      this.state.password
+    ).then(
+      () => {
+        // this.props.hasHistory.push('/home');
+        window.location.href = "/";
+      },
+      (error) => {
+        console.log("signup fail: error = { " + error.toString() + " }");
+        this.setState({
+          error:
+            "Can not signup successfully ! Please check username/password again",
+        });
+      }
+    );
+  };
 
   render() {
-
     return (
       <>
-        <Form />
+        <Navigation />
+        <Form onSubmit={this.doLogin} />
       </>
     );
   }
 }
 
-export default Signup
+export default Signup;
